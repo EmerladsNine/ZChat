277a9f6a526a1289679cf54932191dabbb7a8747
UZMON <nar657965@gmail.com>
2025-12-20 13:27:54 +0200

    better looking search bar

--- lib/main.dart
+++ lib/main.dart
@@ -71,24 +71,33 @@ class _MyHomePageState extends State<MyHomePage> {
           crossAxisAlignment: CrossAxisAlignment.start,
           spacing: 10,
           children: [
-            Text(
-              'Chats',
+
+            Text('Chats',
               style: TextStyle(fontWeight: FontWeight.w900, fontSize: 30),
             ),
+
             //Search Bar
             Container(
               padding: EdgeInsets.fromLTRB(5, 0, 0, 0),
+              height: 35,
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(10.0),
                 color: Colors.grey.shade900,
               ),
-              child: TextField(
-                decoration: InputDecoration(
-                  icon: Icon(Icons.search),
-                  alignLabelWithHint: true,
-                  hintText: 'Ask Zane or Search',
-                  border: InputBorder.none,
-                ),
+              child: Row(
+                children: [
+                  Icon(Icons.search,color: Colors.grey.shade700,),
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        isDense: true,
+                        hintText: 'Ask Zane or Search',
+                        hintStyle: TextStyle(color: Colors.grey.shade700),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
 
@@ -97,41 +106,28 @@ class _MyHomePageState extends State<MyHomePage> {
               onPressed: () {},
               style: ButtonStyle(
                 padding: WidgetStatePropertyAll(EdgeInsets.all(0)),
-                foregroundColor: WidgetStateProperty.all(Colors.grey.shade500),
+                foregroundColor: WidgetStateProperty.all(Colors.grey.shade700),
                 shape: WidgetStateProperty.all(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(5.0),
-                  ),
+                  RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0),),
                 ),
               ),
               child: Row(
                 spacing: 10,
                 children: [
-                  Icon(Icons.chair, size: 70, color: Colors.white),
-
+                  Icon(Icons.chair, size: 50, color: Colors.white),
                   Expanded(
-                    child: Container(
-                      height: 70,
-                      padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
+                    child: Container(height: 70, padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
                       decoration: BoxDecoration(
-                        border: BoxBorder.fromLTRB(
-                          bottom: BorderSide(color: Colors.grey.shade900),
-                        ),
+                        border: BoxBorder.fromLTRB(bottom: BorderSide(color: Colors.grey.shade900),),
                       ),
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                      child: Row(crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Expanded(
                             child: Column(
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                Text(
-                                  'World Chat',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.w600,
-                                    fontSize: 18,
-                                  ),
+                                Text('World Chat',
+                                  style: TextStyle(color: Colors.white, fontWeight: FontWeight.w600, fontSize: 18,),
                                 ),
                                 Text('This will be the last message'),
                               ],
@@ -145,6 +141,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 ],
               ),
             ),
+
           ],
         ),
       ),
