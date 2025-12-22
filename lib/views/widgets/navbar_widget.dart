4023b80228f7b1cf7fbaca8c3c2c1a2295ee74ae
UZMON <nar657965@gmail.com>
2025-12-22 18:27:40 +0200

    nothing

--- lib/views/widgets/navbar_widget.dart
+++ lib/views/widgets/navbar_widget.dart
@@ -9,38 +9,46 @@ class NavbarWidget extends StatelessWidget {
     return ValueListenableBuilder(
       valueListenable: selectedPageNotifier,
       builder: (context, selectedPage, child) {
-        return
-          NavigationBar(
-            backgroundColor: Colors.black,
-            selectedIndex: selectedPage,
-            indicatorColor: Colors.transparent,
-            onDestinationSelected: (val) {
-              selectedPageNotifier.value = val;
-            },
-            destinations: [
-              NavigationDestination(
-                  icon: selectedPage == 0 ? Icon(Icons.star) : Icon(
-                      Icons.star_border),
-                  label: 'Updates'),
-              NavigationDestination(
-                  icon: selectedPage == 1 ? Icon(Icons.call) : Icon(
-                      Icons.call_outlined),
-                  label: 'Calls'),
-              NavigationDestination(
-                icon: selectedPage == 2 ? Icon(Icons.people) : Icon(
-                    Icons.people_outline),
-                label: 'Communities',
-              ),
-              NavigationDestination(
-                  icon: selectedPage == 3 ? Icon(Icons.chat) : Icon(
-                      Icons.chat_outlined),
-                  label: 'Chats'),
-              NavigationDestination(
-                  icon: selectedPage == 4 ? Icon(Icons.settings) : Icon(
-                      Icons.settings_outlined),
-                  label: 'Settings'),
-            ],
-          );
+        return NavigationBar(
+          backgroundColor: Colors.black,
+          selectedIndex: selectedPage,
+          indicatorColor: Colors.transparent,
+          onDestinationSelected: (val) {
+            selectedPageNotifier.value = val;
+          },
+          destinations: [
+            NavigationDestination(
+              icon: selectedPage == 0
+                  ? Icon(Icons.star)
+                  : Icon(Icons.star_border),
+              label: 'Updates',
+            ),
+            NavigationDestination(
+              icon: selectedPage == 1
+                  ? Icon(Icons.call)
+                  : Icon(Icons.call_outlined),
+              label: 'Calls',
+            ),
+            NavigationDestination(
+              icon: selectedPage == 2
+                  ? Icon(Icons.people)
+                  : Icon(Icons.people_outline),
+              label: 'Communities',
+            ),
+            NavigationDestination(
+              icon: selectedPage == 3
+                  ? Icon(Icons.chat)
+                  : Icon(Icons.chat_outlined),
+              label: 'Chats',
+            ),
+            NavigationDestination(
+              icon: selectedPage == 4
+                  ? Icon(Icons.settings)
+                  : Icon(Icons.settings_outlined),
+              label: 'Settings',
+            ),
+          ],
+        );
       },
     );
   }
