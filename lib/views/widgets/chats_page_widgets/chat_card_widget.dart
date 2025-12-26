527e102ed873fbf22ee33126a31f875e17d50d09
Jawad Zaraket <emeraldsnine@gmail.com>
2025-12-26 20:23:34 +0200

    custom animation

--- lib/views/widgets/chats_page_widgets/chat_card_widget.dart
+++ lib/views/widgets/chats_page_widgets/chat_card_widget.dart
@@ -5,7 +5,9 @@ import 'package:zchat/views/data/notifiers.dart';
 import '../../data/text_styles.dart';
 import '../../pages/chat_messages_page.dart';
 
-class ChatCardWidget extends StatelessWidget {
+const int radius = 10;
+
+class ChatCardWidget extends StatefulWidget {
   const ChatCardWidget({
     super.key,
     this.chatName = "Chat Name",
@@ -20,78 +22,118 @@ class ChatCardWidget extends StatelessWidget {
   final String timeStamp;
 
   @override
+  State<ChatCardWidget> createState() {
+    return ChatCardWidgetState();
+  }
+}
+
+class ChatCardWidgetState extends State<ChatCardWidget>{
+  bool pressed = false;
+
+
+  @override
   Widget build(BuildContext context) {
-    return InkWell(
-        splashFactory: InkRipple.splashFactory,
-        onTapDown: (details) {},
-        onTap: () {
+    return GestureDetector(
+      // splashFactory: InkRipple.splashFactory,
+        onTapDown: (details) {
+          setState(() {
+            if(isNavigating.value) return;
+            pressed = true;
+          });
+        },
+        onTap: () async {
           if(isNavigating.value) return;
           isNavigating.value = true;
-          Future.delayed(Duration(milliseconds: 300), () {
-            if(!context.mounted) return;
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) {
-                  return ChatMessagesPage();
-                },
-              ),
-            );
-          }).then((useless) {
-            isNavigating.value = false;
-          },);
-        },
-        borderRadius: BorderRadius.circular(5.0),
-        child: Row(
-          spacing: 10,
-          children: [
-            Container(
-              width: 50.0,
-              height: 50.0,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(50.0),
-                color: cardsColor,
-              ),
-              child: Icon(cardIcon, size: 30, color: primaryColor),
-            ),
-            Expanded(
-              child: Container(
-                height: 70,
-                padding: EdgeInsets.fromLTRB(0, 5, 12.5, 0),
-                decoration: BoxDecoration(
-                  border: BoxBorder.fromLTRB(
-                    bottom: BorderSide(color: dividerColor),
-                  ),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Expanded(
-                            child: Text(
-                              chatName,
-                              style: chatCardNameTextStyle,
-                            ),
-                          ),
-                          Text(timeStamp, style: chatCardMessageDetailsTextStyle),
-                        ]
-                    ),
+          await Future.delayed(Duration(milliseconds: 300));
+          if(!context.mounted) return;
 
-                    Padding(
-                      padding: EdgeInsetsGeometry.only(right: 5),
-                      child: Text(
-                        message,
-                        overflow: TextOverflow.ellipsis,
-                        style:chatCardMessageDetailsTextStyle,
-                      )
+          await Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) {
+                return ChatMessagesPage();
+              },
+            ),
+          ).then((_) {
+            isNavigating.value = false;
+            setState(() {
+              pressed = false; // overlay is now hidden instantly
+            });
+          });
+        },
+        // borderRadius: BorderRadius.circular(5.0),
+        child: Stack(
+            children: [
+              Row(
+                  spacing: 10,
+                  children: [
+                    Container(
+                      width: 50.0,
+                      height: 50.0,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50.0),
+                        color: cardsColor,
+                      ),
+                      child: Icon(widget.cardIcon, size: 30, color: primaryColor),
+                    ),
+                    Expanded(
+                        child: Container(
+                            height: 70,
+                            padding: EdgeInsets.fromLTRB(0, 5, 12.5, 0),
+                            decoration: BoxDecoration(
+                              border: BoxBorder.fromLTRB(
+                                bottom: BorderSide(color: dividerColor),
+                              ),
+                            ),
+                            child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Row(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Expanded(
+                                          child: Text(
+                                            widget.chatName,
+                                            style: chatCardNameTextStyle,
+                                          ),
+                                        ),
+                                        Text(widget.timeStamp, style: chatCardMessageDetailsTextStyle),
+                                      ]
+                                  ),
+
+                                  Padding(
+                                      padding: EdgeInsetsGeometry.only(right: 5),
+                                      child: Text(
+                                        widget.message,
+                                        overflow: TextOverflow.ellipsis,
+                                        style:chatCardMessageDetailsTextStyle,
+                                      )
+                                  )
+                                ]
+                            )
+                        )
                     )
                   ]
+              ),
+              Positioned.fill(
+                child: Center(
+                  child: AnimatedOpacity(
+                      opacity: pressed ? 0.3 : 0,
+                      duration: Duration(milliseconds: 300),
+                      child: AnimatedContainer(
+                        duration: Duration(milliseconds: 300),
+                        curve: Curves.easeOut,
+                        width: pressed ? MediaQuery.of(context).size.width : 0, // grows horizontally
+                        height: double.infinity,
+                        decoration: BoxDecoration(
+                          color: hintColor,
+                          borderRadius: BorderRadius.circular(15)
+                        )
+                      )
+                  )
                 )
               )
-            )
-          ]
+            ]
         )
     );
   }
