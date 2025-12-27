1bfd5b38880b96dae82351a4e32bd53546531e7c
Jawad Zaraket <emeraldsnine@gmail.com>
2025-12-27 14:09:26 +0200

    created custom button classes

---
+++ lib/views/widgets/buttons/base_button_widget.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+abstract class BaseButtonWidget extends StatefulWidget {
+  BaseButtonWidget({
+    super.key,
+    required this.child,
+    this.onTap,
+    this.onTapDown,
+    this.onTapCancel,
+    ValueNotifier<bool>? disableSet,
+    ValueNotifier<bool>? appStateNotifier
+  }) : disableSet = disableSet ?? ValueNotifier(false),
+        appStateNotifier = appStateNotifier ?? ValueNotifier(false);
+
+  final Widget child;
+  final GestureTapCallback? onTap;
+  final GestureTapDownCallback? onTapDown;
+  final GestureTapCancelCallback? onTapCancel;
+  final ValueNotifier<bool>  disableSet;
+  final ValueNotifier<bool> appStateNotifier;
+
+  Widget buildOverlay(BuildContext context, bool pressed, VoidCallback markAnimationAsDone);
+
+  @override
+  State<BaseButtonWidget> createState() {
+    return BaseButtonWidgetState();
+  }
+}
+
+class BaseButtonWidgetState extends State<BaseButtonWidget>{
+  bool _pressed = false;
+  bool _animationDone = false;
+
+  void markAnimationAsDone() {
+    _animationDone = true;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTapDown: (details) {
+        //Only Animate if there is no other animation running.
+        if (widget.disableSet.value) return;
+        widget.disableSet.value = true;
+
+        //Animate
+        _animationDone = false;
+        setState(() {_pressed = true;});
+        widget.onTapDown?.call(details);
+      },
+
+      onTapCancel: () {
+        //Consider canceling navigation if the Cancelled tap is the one that is making navigation.
+        if(!_pressed || widget.appStateNotifier.value ) return;
+
+        //reset
+        widget.disableSet.value = false;
+        setState(() {_pressed = false;});
+
+        widget.onTapCancel?.call();
+      },
+
+      onTap: () async {
+        //Only Navigate if there is no other navigation running and you are the button being animated.
+        if (!_pressed || widget.appStateNotifier.value) return;
+        widget.appStateNotifier.value = true;
+
+        //Keep waiting as long as the animation running , or just stop everything if context is no longer mounted .
+        while (!_animationDone && context.mounted) {
+          await Future.delayed(Duration(milliseconds: 30));
+        }
+
+        //reset
+        widget.appStateNotifier.value = false;
+        widget.disableSet.value = false;
+        _pressed = false; // overlay is now hidden instantly
+
+        if (!context.mounted) return;
+
+        //context is mounted , now refresh everything.
+        setState(() {});
+
+        widget.onTap?.call();
+      },
+      child: Stack(
+        children: [
+          widget.child,
+          widget.buildOverlay(context, _pressed, markAnimationAsDone)
+        ]
+      )
+    );
+  }
+}
--- lib/views/widgets/chats_page_widgets/chat_card_widget.dart
+++ lib/views/widgets/chats_page_widgets/chat_card_widget.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:zchat/views/data/colors.dart';
 import 'package:zchat/views/data/notifiers.dart';
+import 'package:zchat/views/widgets/buttons/ripple_effect_button_widget.dart';
 
 import '../../data/text_styles.dart';
 import '../../pages/chat_messages_page.dart';
 
-const int radius = 10;
-bool disableChatCards = false;
+ValueNotifier<bool> disableChatCards = ValueNotifier(false);
 
-class ChatCardWidget extends StatefulWidget {
+class ChatCardWidget extends StatelessWidget {
   const ChatCardWidget({
     super.key,
     this.chatName = "Chat Name",
@@ -23,147 +23,73 @@ class ChatCardWidget extends StatefulWidget {
   final String timeStamp;
 
   @override
-  State<ChatCardWidget> createState() {
-    return ChatCardWidgetState();
-  }
-}
-
-class ChatCardWidgetState extends State<ChatCardWidget> {
-  final Duration _animationTime = Duration(milliseconds: 200);
-
-  bool _pressed = false;
-  bool _animationDone = false;
-
-  @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTapDown: (details) {
-        //Only Animate if there is no other animation running.
-        if (disableChatCards) return;
-        disableChatCards = true;
-
-        //Animate
-        _animationDone = false;
-        setState(() {_pressed = true;});
-      },
-
-      onTapCancel: () {
-        //Consider canceling navigation if the Cancelled tap is the one that is making navigation.
-        if(!_pressed || isNavigating.value ) return;
-
-        //reset
-        disableChatCards = false;
-        setState(() {_pressed = false;});
-      },
-
-      onTap: () async {
-        //Only Navigate if there is no other navigation running and you are the button being animated.
-        if (!_pressed || isNavigating.value) return;
-        isNavigating.value = true;
-
-        //Keep waiting as long as the animation running , or just stop everything if context is no longer mounted .
-        while (!_animationDone && context.mounted) {
-          await Future.delayed(Duration(milliseconds: 30));
-        }
-
-        //reset
-        isNavigating.value = false;
-        disableChatCards = false;
-        _pressed = false; // overlay is now hidden instantly
-
-        if (!context.mounted) return;
-
-        //context is mounted , now refresh everything and navigate.
-        setState(() {});
-
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) {
-              return ChatMessagesPage();
-            },
-          ),
-        );
-      },
-      child: Stack(
-        children: [
-          Row(
+    return RippleEffectButtonWidget(
+        disableSet: disableChatCards,
+        appStateNotifier: isNavigating,
+        onTap: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) {
+                    return ChatMessagesPage();
+                  }
+              )
+          );
+        },
+        child: Row(
             spacing: 10,
             children: [
               Container(
-                width: 50.0,
-                height: 50.0,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(50.0),
-                  color: cardsColor,
-                ),
-                child: Icon(widget.cardIcon, size: 30, color: primaryColor),
+                  width: 50.0,
+                  height: 50.0,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50.0),
+                      color: cardsColor
+                  ),
+                  child: Icon(cardIcon, size: 30, color: primaryColor)
               ),
               Expanded(
-                child: Container(
-                  height: 70,
-                  padding: EdgeInsets.fromLTRB(0, 5, 12.5, 0),
-                  decoration: BoxDecoration(
-                    border: BoxBorder.fromLTRB(
-                      bottom: BorderSide(color: dividerColor),
-                    ),
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Expanded(
-                            child: Text(
-                              widget.chatName,
-                              style: chatCardNameTextStyle,
+                  child: Container(
+                      height: 70,
+                      padding: EdgeInsets.fromLTRB(0, 5, 12.5, 0),
+                      decoration: BoxDecoration(
+                          border: BoxBorder.fromLTRB(
+                              bottom: BorderSide(color: dividerColor)
+                          )
+                      ),
+                      child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Expanded(
+                                      child: Text(
+                                          chatName,
+                                          style: chatCardNameTextStyle
+                                      )
+                                  ),
+                                  Text(
+                                      timeStamp,
+                                      style: chatCardMessageDetailsTextStyle
+                                  )
+                                ]
                             ),
-                          ),
-                          Text(
-                            widget.timeStamp,
-                            style: chatCardMessageDetailsTextStyle,
-                          ),
-                        ],
-                      ),
-
-                      Padding(
-                        padding: EdgeInsetsGeometry.only(right: 5),
-                        child: Text(
-                          widget.message,
-                          overflow: TextOverflow.ellipsis,
-                          style: chatCardMessageDetailsTextStyle,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
-          Positioned.fill(
-            child: Center(
-              child: AnimatedOpacity(
-                opacity: _pressed ? 0.3 : 0,
-                duration: _pressed ? _animationTime : Duration.zero,
-                child: AnimatedContainer(
-                  duration: _pressed ? _animationTime : Duration.zero,
-                  onEnd: () => _animationDone = true,
-                  curve: Curves.easeOut,
-                  width: _pressed
-                      ? MediaQuery.of(context).size.width
-                      : 0, // grows horizontally
-                  height: double.infinity,
-                  decoration: BoxDecoration(
-                    color: hintColor,
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
+                            Padding(
+                                padding: EdgeInsetsGeometry.only(right: 5),
+                                child: Text(
+                                    message,
+                                    overflow: TextOverflow.ellipsis,
+                                    style: chatCardMessageDetailsTextStyle
+                                )
+                            )
+                          ]
+                      )
+                  )
+              )
+            ]
+        )
     );
   }
 }
