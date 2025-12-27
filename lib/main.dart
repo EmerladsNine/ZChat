793241eb554b0e43de8d36955df4382f2feef5dd
UZMON <nar657965@gmail.com>
2025-12-27 14:04:13 +0200

    smoother swiping

--- lib/main.dart
+++ lib/main.dart
@@ -60,9 +60,9 @@ class _MyHomePageState extends State<MyHomePage> {
     pageController = PageController(initialPage: selectedPageNotifier.value);
     fullSwipeController = FullSwipeController(
       pageController: pageController,
-      minSwipeDistance: 100,
+      minSwipeDistance: 10,
       minSwipeVelocity: 1,
-      snapAnimationDuration: Duration(milliseconds: 300),
+      snapAnimationDuration: Duration(milliseconds: 200),
       snapCurve: Curves.ease,
     );
   }
