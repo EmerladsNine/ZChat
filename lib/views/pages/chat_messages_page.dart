65b5506ec81d243c7787147755147ec72bfc9082
UZMON <nar657965@gmail.com>
2025-12-30 13:04:59 +0200

    designed a message bubble

--- lib/views/pages/chat_messages_page.dart
+++ lib/views/pages/chat_messages_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:zchat/views/data/colors.dart';
+import 'package:zchat/views/data/message_reply_data.dart';
 import 'package:zchat/views/widgets/chatting_page_widgets/chat_messages_footer_widget.dart';
+import 'package:zchat/views/widgets/chatting_page_widgets/message_bubble.dart';
 
 class ChatMessagesPage extends StatelessWidget {
   const ChatMessagesPage({super.key});
@@ -13,9 +15,7 @@ class ChatMessagesPage extends StatelessWidget {
         fit: StackFit.expand,
         children: [
           //BackgroundImageFallBack
-          Positioned.fill(child: Container(
-            color: primaryBackgroundColor,
-          )),
+          Positioned.fill(child: Container(color: primaryBackgroundColor)),
           Image(
             image: Image.asset('assets/images/bg4.jpeg').image,
             fit: BoxFit.cover,
@@ -24,17 +24,58 @@ class ChatMessagesPage extends StatelessWidget {
           ),
           Column(
             children: [
-              Expanded(child: Container()),
+              Expanded(
+                child: SafeArea(
+                  bottom: false,
+                  child: SingleChildScrollView(
+                    padding: EdgeInsets.only(left: 5,right: 15),
+                      child: LayoutBuilder(
+                        builder:(context, constraints) {
+                          final double maxWidth = constraints.maxWidth * 0.7;
+                          return Column(
+                              spacing: 5,
+                              children: [
+                                SizedBox(height: 20,),
+                                MessageBubble(text: "Hello bob",time: "12:00 PM",senderName: 'Max',maxBubbleWidth: maxWidth,),
+                                MessageBubble(text: "Hello max",time: "12:00 PM",maxBubbleWidth: maxWidth,),
+                                MessageBubble(text: "How are you mr bob are you fine ?",senderName: 'Max',time: "12:00 PM",maxBubbleWidth: maxWidth,),
+                                MessageBubble(text: "yes I am , How are you mr max",time: "12:00 PM",maxBubbleWidth: maxWidth,),
+                                MessageBubble(text: "good",time: "12:00 PM",senderName: 'Max',maxBubbleWidth: maxWidth,),
+                                MessageBubble(text: "bye bob",time: "12:00 PM",senderName: 'Max',maxBubbleWidth: maxWidth,),
+                                MessageBubble(text: "bye max",time: "12:00 PM",maxBubbleWidth: maxWidth,replyData: MessageReplyData('bye bob', 'Max'),),
+                                MessageBubble(text: "maaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaax HELPPPP",time: "12:00 PM",maxBubbleWidth: maxWidth,replyData: MessageReplyData('bye bob', 'Max'),),
+                                MessageBubble(text: "Hello bob",time: "12:00 PM",senderName: 'Max',maxBubbleWidth: maxWidth,),
+                                MessageBubble(text: "Hello max",time: "12:00 PM",maxBubbleWidth: maxWidth,),
+                                MessageBubble(text: "How are you mr bob are you fine ?",senderName: 'Max',time: "12:00 PM",maxBubbleWidth: maxWidth,),
+                                MessageBubble(text: "yes I am , How are you mr max",time: "12:00 PM",maxBubbleWidth: maxWidth,),
+                                MessageBubble(text: "good",time: "12:00 PM",senderName: 'Max',maxBubbleWidth: maxWidth,),
+                                MessageBubble(text: "bye bob",time: "12:00 PM",senderName: 'Max',maxBubbleWidth: maxWidth,),
+                                MessageBubble(text: "bye max",time: "12:00 PM",maxBubbleWidth: maxWidth,replyData: MessageReplyData('bye bob', 'Max'),),
+                                MessageBubble(text: "maaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaax HELPPPP",time: "12:00 PM",maxBubbleWidth: maxWidth,replyData: MessageReplyData('bye bob', 'Max'),),
+                                MessageBubble(text: "Hello bob",time: "12:00 PM",senderName: 'Max',maxBubbleWidth: maxWidth,),
+                                MessageBubble(text: "Hello max",time: "12:00 PM",maxBubbleWidth: maxWidth,),
+                                MessageBubble(text: "How are you mr bob are you fine ?",replyData: MessageReplyData('Hello max', 'You'),senderName: 'Max',time: "12:00 PM",maxBubbleWidth: maxWidth,),
+                                MessageBubble(text: "yes I am , How are you mr max",time: "12:00 PM",maxBubbleWidth: maxWidth,),
+                                MessageBubble(text: "good",time: "12:00 PM",senderName: 'Max',maxBubbleWidth: maxWidth,),
+                                MessageBubble(text: "bye bob",time: "12:00 PM",senderName: 'Max',maxBubbleWidth: maxWidth,),
+                                MessageBubble(text: "bye max",time: "12:00 PM",maxBubbleWidth: maxWidth,replyData: MessageReplyData('bye bob', 'Max'),),
+                                MessageBubble(text: "maaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaax HELPPPP",time: "12:00 PM",maxBubbleWidth: maxWidth,replyData: MessageReplyData('bye bob', 'Max'),),
+                                SizedBox(height: 10,)
+                                            ]);
+                        }
+                      )),
+                ),
+              ),
               Padding(
                 padding: EdgeInsets.only(
-                  bottom: MediaQuery.of(context).viewInsets.bottom
+                  bottom: MediaQuery.of(context).viewInsets.bottom,
                 ),
                 child: ChatMessagesFooterWidget(),
               ),
             ],
           ),
         ],
-      )
+      ),
     );
   }
 }
