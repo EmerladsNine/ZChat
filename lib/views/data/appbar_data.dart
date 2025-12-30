c47e8461391c4c1fb88b9a00314f28490b521fdf
Jawad Zaraket <emeraldsnine@gmail.com>
2025-12-30 15:45:13 +0200

    fixed typo

--- lib/views/data/appbar_data.dart
+++ lib/views/data/appbar_data.dart
@@ -3,7 +3,7 @@ import 'package:zchat/views/widgets/chats_page_widgets/chats_page_appbar_actions
 import '../data_classes/appbar_config.dart';
 
 final List<AppBarConfig> appBarData = const [
-  AppBarConfig(title: 'Chats', actions: ChatsPageAppbarActionsWidget()),
+  AppBarConfig(title: 'Chat', actions: ChatsPageAppbarActionsWidget()),
   AppBarConfig(title: 'Posts'),
   AppBarConfig(title: 'ane'), // Zane without the Z
   AppBarConfig(title: 'Calls'),
