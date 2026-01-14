9c353067b092fff94c5aecba4dbcd6bb32f7a9cd
UZMON <nar657965@gmail.com>
2026-01-14 13:24:06 +0200

    fixed spacing

--- lib/views/widgets/chatting_page_widgets/message_bubble.dart
+++ lib/views/widgets/chatting_page_widgets/message_bubble.dart
@@ -20,95 +20,100 @@ class MessageBubble extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: senderName == null
-          ? MainAxisAlignment.end
-          : MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      spacing: 5,
-      children: [
-        if (senderName != null)
-          Container(
-            decoration: BoxDecoration(
-              color: AppThemes.darkThemeColors.cardsColor,
-              borderRadius: BorderRadius.circular(30),
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 2,horizontal: 5),
+      child: Row(
+        mainAxisAlignment: senderName == null
+            ? MainAxisAlignment.end
+            : MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        spacing: 5,
+        children: [
+          if (senderName != null)
+            Container(
+              decoration: BoxDecoration(
+                color: AppThemes.darkThemeColors.cardsColor,
+                borderRadius: BorderRadius.circular(30),
+              ),
+              width: 40,
+              height: 40,
+              child: Icon(Icons.person),
             ),
-            width: 40,
-            height: 40,
-            child: Icon(Icons.person),
-          ),
-        IntrinsicWidth(
-          child: Container(
-            constraints: BoxConstraints(minWidth: 50, maxWidth: maxBubbleWidth),
-            decoration: BoxDecoration(
-              color: AppThemes.darkThemeColors.cardsColor,
-              borderRadius: BorderRadius.circular(10),
-              border: Border.all(color: AppThemes.darkThemeColors.dividerColor),
-            ),
-            padding: EdgeInsets.all(5),
-            margin: senderName == null
-                ? EdgeInsets.symmetric(vertical: 5)
-                : EdgeInsets.zero,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                if (senderName != null)
-                  Text(
-                    senderName!,
-                    style: TextStyle(
-                      fontWeight: FontWeight.w900,
-                      fontSize: 15,
-                      color: AppThemes.darkThemeColors.brandPrimaryColor,
-                    ),
-                  ),
-                if (replyData != null)
-                  Container(
-                    padding: EdgeInsets.all(5),
-                    decoration: BoxDecoration(
-                      color: AppThemes.darkThemeColors.dividerColor,
-                      borderRadius: BorderRadius.circular(5),
-                      border: BoxBorder.fromLTRB(
-                        left: BorderSide(color: Colors.white, width: 3),
-                      ),
-                    ),
-                    child: Row(
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              replyData!.replyTextSender,
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                            Text(
-                              replyData!.replyText,
-                              style: TextStyle(
-                                color: AppThemes.darkThemeColors.hintColor,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                Text(text, style: TextStyle(fontSize: 17)),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
+          IntrinsicWidth(
+            child: Container(
+              constraints: BoxConstraints(minWidth: 50, maxWidth: maxBubbleWidth),
+              decoration: BoxDecoration(
+                color: AppThemes.darkThemeColors.cardsColor,
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(color: AppThemes.darkThemeColors.dividerColor),
+              ),
+              padding: EdgeInsets.symmetric(vertical: 5,horizontal: 5),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+
+                  if (senderName != null)
                     Text(
-                      time,
+                      senderName!,
                       style: TextStyle(
-                        color: AppThemes.darkThemeColors.primaryColor,
-                        fontSize: 10,
+                        fontWeight: FontWeight.w900,
+                        fontSize: 15,
+                        color: AppThemes.darkThemeColors.brandPrimaryColor,
+                        height: 1
                       ),
                     ),
-                  ],
-                ),
-              ],
+
+                  if (replyData != null)
+                    Container(
+                      padding: EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                        color: AppThemes.darkThemeColors.dividerColor,
+                        borderRadius: BorderRadius.circular(5),
+                        border: BoxBorder.fromLTRB(
+                          left: BorderSide(color: Colors.white, width: 3),
+                        ),
+                      ),
+                      child: Row(
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                replyData!.replyTextSender,
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                replyData!.replyText,
+                                style: TextStyle(
+                                  color: AppThemes.darkThemeColors.hintColor,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+
+                  Text(text, style: TextStyle(fontSize: 17 , height: 1)),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        time,
+                        style: TextStyle(
+                          color: AppThemes.darkThemeColors.primaryColor,
+                          fontSize: 10,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
