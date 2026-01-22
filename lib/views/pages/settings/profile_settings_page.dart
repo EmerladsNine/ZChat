ee21784f421b3e0b74e6be96d18d5452416d2db7
Jawad Zaraket <emeraldsnine@gmail.com>
2026-01-22 11:02:01 +0200

    remove unused import

--- lib/views/pages/settings/profile_settings_page.dart
+++ lib/views/pages/settings/profile_settings_page.dart
@@ -3,7 +3,6 @@ import 'package:zchat/views/data/app_text_styles.dart';
 import 'package:zchat/views/widgets/settings_widgets/profile_info_card_widget.dart';
 
 import '../../../themes_system/app_theme.dart';
-import '../../data/app_themes.dart';
 import 'base_settings_page.dart';
 
 /// Page for viewing and editing profile information.
