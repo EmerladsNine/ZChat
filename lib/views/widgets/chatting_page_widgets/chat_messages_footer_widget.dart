a2daddf97fa22da6d6f00410fc8c41ac9e3c1d75
Jawad Zaraket <emeraldsnine@gmail.com>
2026-01-22 16:47:46 +0200

    margin addition to chat footer bcz msg feels so close

--- lib/views/widgets/chatting_page_widgets/chat_messages_footer_widget.dart
+++ lib/views/widgets/chatting_page_widgets/chat_messages_footer_widget.dart
@@ -9,7 +9,8 @@ import 'camera_button_widget.dart';
 import 'emoji_panel_button_widget.dart';
 
 class ChatMessagesFooterWidget extends StatefulWidget {
-  const ChatMessagesFooterWidget({super.key,required this.scrollController});
+  const ChatMessagesFooterWidget({super.key, required this.scrollController});
+
   final ScrollController scrollController;
 
   @override
@@ -34,6 +35,7 @@ class _ChatMessagesFooterWidgetState extends State<ChatMessagesFooterWidget> {
 
     return Container(
       color: colors.cardsColor,
+      margin: EdgeInsetsGeometry.only(top: 5),
       child: SafeArea(
         top: false,
         child: Material(
@@ -52,7 +54,10 @@ class _ChatMessagesFooterWidgetState extends State<ChatMessagesFooterWidget> {
                 ChatTextFieldWidget(controller: controller),
                 AddFileWidget(),
                 CameraButtonWidget(),
-                SendButtonWidget(controller: controller,scrollController: widget.scrollController,),
+                SendButtonWidget(
+                  controller: controller,
+                  scrollController: widget.scrollController,
+                ),
               ],
             ),
           ),
