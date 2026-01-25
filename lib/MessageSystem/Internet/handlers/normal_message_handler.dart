1769a2246c847e97d987f08138955af776f688e4
Jawad Zaraket <emeraldsnine@gmail.com>
2026-01-25 16:57:17 +0200

    added timeStamp

--- lib/MessageSystem/Internet/handlers/normal_message_handler.dart
+++ lib/MessageSystem/Internet/handlers/normal_message_handler.dart
@@ -7,9 +7,17 @@ import 'package:zchat/utils/print_on_debug.dart';
 
 class NormalMessageHandler extends Handler {
   @override
-  bool handle(List<int> buffer,MessagingService service) {
+  bool handle(List<int> buffer, MessagingService service) {
+    int timeStamp = 0;
+    for (int i = 0; i < 8; i++) {
+      timeStamp = timeStamp << 8 | buffer[i];
+    }
+    buffer.removeRange(0, 8);
+
     final String response = utf8.decode(buffer);
-    currentChat?.addMessage(Message(text: response, senderName: "Max"));
+    currentChat?.addMessage(
+      Message(text: response, senderName: "Max", timestamp: timeStamp),
+    );
     printOnDebug('Server: $response');
     return true;
   }
--- lib/MessageSystem/Internet/listener_service.dart
+++ lib/MessageSystem/Internet/listener_service.dart
@@ -7,47 +7,47 @@ import 'package:zchat/MessageSystem/Internet/messaging_service.dart';
 
 class ListenerService {
   ListenerService(this.messagingService);
+
   final MessagingService messagingService;
 
   Map<MessageType, Handler> handlers = {
-    MessageType.ping : PingHandler(),
-    MessageType.pong : PongHandler(),
-    MessageType.normalMessage : NormalMessageHandler()
+    MessageType.ping: PingHandler(),
+    MessageType.pong: PongHandler(),
+    MessageType.normalMessage: NormalMessageHandler(),
   };
 
   MessageType? head;
   int? expectedLength;
+  int timeStamp = 0;
   List<int> buffer = [];
 
   //returns if the unit was received completely.
-  bool processProtocolUnit()
-  {
-      if(head == null) {
-        head = MessageType.fromId(buffer[0]);
-        buffer.removeAt(0);
+  bool processProtocolUnit() {
+    if (head == null) {
+      head = MessageType.fromId(buffer[0]);
+      buffer.removeAt(0);
+    }
+
+    if (head!.hasBody) {
+      if (expectedLength == null) {
+        if (buffer.length < 2) return false;
+        expectedLength = (buffer[0] << 8) | buffer[1];
+        buffer.removeRange(0, 2);
       }
+      if (buffer.length < expectedLength!) return false;
+    }
 
-      if(head!.hasBody) {
-        if (expectedLength == null) {
-          if (buffer.length < 2) return false;
-          expectedLength = (buffer[0] << 8) | buffer[1];
-          buffer.removeRange(0, 2);
-        }
-        if (buffer.length < expectedLength!) return false;
-      }
+    handlers[head]!.handle(buffer.sublist(0, expectedLength), messagingService);
 
-      handlers[head]!.handle(buffer.sublist(0,expectedLength), messagingService);
-
-      head = null;
-      expectedLength = null;
-      buffer = [];
-      return true;
+    head = null;
+    expectedLength = null;
+    buffer = [];
+    return true;
   }
 
-  void processBuffer()
-  {
-    while(buffer.isNotEmpty) {
-        if(!processProtocolUnit()) break;
+  void processBuffer() {
+    while (buffer.isNotEmpty) {
+      if (!processProtocolUnit()) break;
     }
   }
 
