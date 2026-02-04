adc8d5b7abfe4aaf4baf840a4e9afa1d78e3e6af
Jawad Zaraket <emeraldsnine@gmail.com>
2026-02-04 18:22:33 +0200

    better colors for the chatting page

--- lib/views/widgets/chatting_page_widgets/message_bubble_widgets/message_bubble_reply_section_widget.dart
+++ lib/views/widgets/chatting_page_widgets/message_bubble_widgets/message_bubble_reply_section_widget.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:zchat/enums/message_bubble_color.dart';
 import 'package:zchat/themes_system/theme_controller.dart';
-import 'package:zchat/views/data/app_message_bubble_colors.dart';
 
 import '../../../../themes_system/app_theme.dart';
 import '../../../../themes_system/theme_color_scheme.dart';
@@ -24,28 +22,13 @@ class MessageBubbleReplySectionWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     final ThemeController themeController = AppTheme.controllerOf(context);
     final ThemeColorScheme colors = themeController.colors;
-    final List<MessageBubbleColor> messageBubbleColors =
-        themeController.messageBubbleColors;
-    final int alpha = (themeController.opacity * 255).round();
     return Container(
       padding: isSeparate ? EdgeInsetsGeometry.all(4) : EdgeInsetsGeometry.zero,
-      decoration: BoxDecoration(
-        color: received
-            ? AppMessageBubbleColors.get(
-                messageBubbleColors[1],
-                themeController.isDarkMode,
-              ).withAlpha(alpha)
-            : AppMessageBubbleColors.get(
-                messageBubbleColors[0],
-                themeController.isDarkMode,
-              ).withAlpha(alpha),
-        borderRadius: BorderRadius.circular(8),
-      ),
       child: Container(
         padding: EdgeInsets.all(5),
         constraints: BoxConstraints(minWidth: 100),
         decoration: BoxDecoration(
-          color: colors.primaryColor.withAlpha(40),
+          color: colors.primaryBackgroundColor.withAlpha(60),
           borderRadius: BorderRadius.circular(5),
           border: BoxBorder.fromLTRB(
             left: BorderSide(color: colors.primaryColor, width: 3),
