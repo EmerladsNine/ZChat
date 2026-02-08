8d1e08f94a95ee484741b3b099501700bbbc089c
Jawad Zaraket <emeraldsnine@gmail.com>
2026-02-08 20:19:18 +0200

    addition of some chat actions

--- lib/views/data/pages_data/chat_details_page_data.dart
+++ lib/views/data/pages_data/chat_details_page_data.dart
@@ -1,3 +1,6 @@
+import 'package:zchat/views/data_classes/pages/settings/settings_navigation_button_data.dart';
+import 'package:zchat/views/pages/temp.dart';
+
 import '../../data_classes/pages/settings/settings_base_button_data.dart';
 import '../../data_classes/pages/settings/settings_on_tap_button_data.dart';
 
@@ -9,11 +12,9 @@ final List<SettingsBaseButtonData> chatSettingsData = [
     },
     icon: '⭐',
   ),
-  SettingsOnTapButtonData(
-    label: 'Mute Notifications',
-    onTap: () async {
-      // Handle mute notifications tap
-    },
-    icon: '🔕',
+  SettingsNavigationButtonData(
+    label: 'Notifications',
+    goToPage: TempPage(),
+    icon: '🔔',
   ),
 ];
--- lib/views/pages/chat_details_page.dart
+++ lib/views/pages/chat_details_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:zchat/messages_system/data_classes/chat_data.dart';
 import 'package:zchat/messages_system/data_classes/group_chat_data.dart';
+import 'package:zchat/views/data_classes/pages/settings/settings_switch_data.dart';
 import 'package:zchat/views/widgets/chat_details_widgets/chat_details_description_widget.dart';
+import 'package:zchat/views/widgets/settings_widgets/settings_switch_widget.dart';
 
 import '../../messages_system/data_classes/private_chat_data.dart';
 import '../../themes_system/app_theme.dart';
@@ -49,6 +51,15 @@ class ChatDetailsPage extends StatelessWidget {
                     SettingsCardWidget(
                       category: 'Chat Settings',
                       buttons: chatSettingsData,
+                    ),
+
+                    SettingsSwitchWidget(
+                      data: SettingsSwitchData(
+                        label: 'Media Visibility',
+                        helpText:
+                            'Show newly downloaded media in your device gallery.',
+                        icon: '🖼️',
+                      ),
                     ),
 
                     if (isGrp)
