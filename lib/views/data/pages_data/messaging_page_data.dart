e40f3436499e9596b36b29cbcbadede59c7ec0b7
UZMON <nar657965@gmail.com>
2026-03-03 16:20:40 +0200

    implemented reply message to the messages action menu

--- lib/views/data/pages_data/messaging_page_data.dart
+++ lib/views/data/pages_data/messaging_page_data.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
+import 'package:zchat/messages_system/data_classes/message_reply_data.dart';
 import 'package:zchat/views/data/app_constants.dart';
 import 'package:zchat/views/data/app_notifiers.dart';
 import 'package:zchat/views/data_classes/pages/ripple_effect_button_data.dart';
+import 'package:zchat/views/overlays/message_actions_menu_widget.dart';
 import 'package:zchat/views/widgets/chatting_page_widgets/message_action_item_widget.dart';
 
 final List<RippleEffectButtonData> commonMessageActionsData = [
@@ -10,13 +12,18 @@ final List<RippleEffectButtonData> commonMessageActionsData = [
     overlayBorderRadius: BorderRadius.vertical(
       top: Radius.circular(AppConstants.messageActionsMenuBorderRadius),
     ),
-    onTap: () {},
+    onTap: () {
+      if(AppNotifiers.selectedMessage.value == null) return;
+      AppNotifiers.replyData.value = MessageReplyData(AppNotifiers.selectedMessage.value!.text, AppNotifiers.selectedMessage.value!.senderName ?? "You");
+      MessageActionsMenuWidget.instance.removeOverlay();
+      },
     child: MessageActionItemWidget(label: 'Reply', icon: '↩️'),
   ),
   RippleEffectButtonData(
     onTap: () {
       if(AppNotifiers.selectedMessage.value == null) return;
       Clipboard.setData(ClipboardData(text: AppNotifiers.selectedMessage.value!.text));
+      MessageActionsMenuWidget.instance.removeOverlay();
     },
     child: MessageActionItemWidget(label: 'Copy', icon: '📄'),
   ),
@@ -32,6 +39,10 @@ final List<RippleEffectButtonData> commonMessageActionsData = [
     onTap: () {},
     child: MessageActionItemWidget(label: 'Star', icon: '⭐'),
   ),
+  RippleEffectButtonData(
+    onTap: () {},
+    child: MessageActionItemWidget(label: 'Delete', icon: '🗑️'),
+  ),
 ];
 
 final List<RippleEffectButtonData> sentMessageActionsData = [
@@ -39,10 +50,6 @@ final List<RippleEffectButtonData> sentMessageActionsData = [
   RippleEffectButtonData(
     onTap: () {},
     child: MessageActionItemWidget(label: 'Edit', icon: '✏️'),
-  ),
-  RippleEffectButtonData(
-    onTap: () {},
-    child: MessageActionItemWidget(label: 'Delete', icon: '🗑️'),
   ),
 ];
 
