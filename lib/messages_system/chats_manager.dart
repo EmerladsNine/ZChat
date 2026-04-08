dc4e59187779550a30172de8294c46c982ab565e
UZMON <nar657965@gmail.com>
2026-04-08 13:22:45 +0300

    changed the background resolution of the chat

--- lib/messages_system/chats_manager.dart
+++ lib/messages_system/chats_manager.dart
@@ -10,7 +10,7 @@ class ChatsManager extends ChangeNotifier {
   Map<int, String> usernames = {0: "You"};
   List<Chat> openedChats = [];
   List<Chat> pinnedChats = [];
-  final defaultBackgroundProvider = const AssetImage('assets/images/background.webp');
+  final defaultBackgroundProvider = const AssetImage('assets/images/background_1080X1920.webp');
   
   void requestUsername(ServerApi api, int userId) async {
     SearchEvent? result = await api.protocolSender.search.searchByIdAsync(
